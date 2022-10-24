fb01c13ca658e7a276ac14c8248e0151bf703bce
411730723 <116533192+411730723@users.noreply.github.com>
2022-10-24 20:49:36 +0800

    Add files via upload

--- 20221024/sketch.js
+++ 20221024/sketch.js
@@ -1,21 +1,26 @@
-function setup(){
-  createCanvas(windowWidth,windowHeight);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
   background(0);
-}
-
-function draw(){
+  }
   
-  rectMode(CENTER)
-  noFill()
-  stroke(255)
-  strokeWeight(1)
-  for(var j=0;j<int(height/(50));j++)
-      {
-          for(var i=0;i<int(width/(49));i++)
-              {
-          ellipse(25+(i*100),25+(j*100),50)
-          rect(25+(i*100),25+(j*100),50)
-          ellipse(50+(i*100),50+(j*100),20)
-              }
-      }
-}
+  function draw() {
+   
+    rectMode(CENTER)//設定方框的座標點為中心點
+    noFill()//不充滿顏色
+    //stroke(255)
+   
+    background(0)
+    for(var j=0;j<int(height/(49));j++)
+  {
+    for(var i=0;i<int(width/(48));i++)
+        {
+  stroke(255,0,0)
+  ellipse(25+(i*90),25+(j*90),50+(mouseX+mouseY)/40)
+  stroke(255,255,0)
+    rect(25+(i*90),25+(j*90),50+(mouseX+mouseY)/40)
+  stroke(0,0,255)
+    ellipse(50+(i*90),50+(j*90),20+(mouseX+mouseY)/40)
+     }
+   }
+  }
+  
